aa602c6c69954024eb927e9d54805602e067623f
Adam Fehnel <link293@gmail.com>
2018-12-19 20:53:26 -0500

    Putting in the PetPoint iframe code for a few pages.
    Signed-off-by: Adam Fehnel <link293@gmail.com>

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -14,19 +14,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<header class="entry-header">
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( '<h1 class="entry-title pb-3">', '</h1>' ); ?>
 
 		<div class="entry-meta">
 
-			<?php understrap_posted_on(); ?>
+			<?php /* understrap_posted_on(); */ ?>
 
 		</div><!-- .entry-meta -->
 
 	</header><!-- .entry-header -->
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+	<?php echo get_the_post_thumbnail( $post->ID, 'full' ); ?>
 
-	<div class="entry-content">
+	<div class="entry-content pt-5">
 
 		<?php the_content(); ?>
 
@@ -41,7 +41,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<footer class="entry-footer">
 
-		<?php understrap_entry_footer(); ?>
+		<?php if ( in_category( 'cats' ) ) : ?>
+			<hr class="my-5">
+			<h3 class="pb-3">Adoptable this and/or other cats below</h3>
+			<iframe src="http://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals2.aspx?species=Cat&amp;gender=A&amp;agegroup=All&amp;location=&amp;site=&amp;onhold=A&amp;orderby=ID&amp;colnum=3&amp;css=&amp;authkey=xs2ff5ungpwuh2whi5va85f0ws3he13qo0tcw2fouaaw48jfjx&amp;recAmount=&amp;detailsInPopup=No&amp;featuredPet=Include&amp;stageID=" scrolling="auto" frameborder="0" class="full-size"></iframe>
+		<?php elseif ( in_category( 'dogs' ) ) : ?>
+			<hr class="my-5">
+			<h3 class="pb-3">Adoptable this and/or other dogs below</h3>
+			<iframe src="http://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals2.aspx?species=Dog&amp;gender=A&amp;agegroup=All&amp;location=&amp;site=&amp;onhold=A&amp;orderby=ID&amp;colnum=3&amp;css=&amp;authkey=xs2ff5ungpwuh2whi5va85f0ws3he13qo0tcw2fouaaw48jfjx&amp;recAmount=&amp;detailsInPopup=No&amp;featuredPet=Include&amp;stageID=" scrolling="auto" frameborder="0" class="full-size"></iframe>
+		<?php endif; ?>
+
+		<?php /* understrap_entry_footer(); */ ?>
 
 	</footer><!-- .entry-footer -->
 
