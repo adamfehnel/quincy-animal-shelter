a322fdacd2d7a966efdbc40ffd999ab7d9ebfdec
Adam Fehnel <link293@gmail.com>
2019-01-19 15:12:14 -0500

    Put images on the side
    Signed-off-by: Adam Fehnel <link293@gmail.com>

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -24,17 +24,46 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	</header><!-- .entry-header -->
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'full' ); ?>
-
 	<div class="entry-content pt-5">
 
-		<?php the_content(); ?>
+		<?php if ( !has_post_thumbnail() ) : ?>
+			<?php the_content(); ?>
+		<?php else: ?>
+
+			<div class="row">
+				<div class="col-md-7">
+					<?php the_content(); ?>
+				</div>
+				<div class="col-md-5">
+					<?php echo get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'w-100' ) ); ?>
+
+					<?php 
+					if ( class_exists('Dynamic_Featured_Image') ) {
+						global $dynamic_featured_image;
+						$additional_featured_images = $dynamic_featured_image->get_featured_images( );
+						?>
+						<div class="row">
+						<?php foreach( $additional_featured_images as $additional_featured_image ): ?>
+							<div class="col-6 mt-4">
+								<a href="<?php echo $additional_featured_image['full'] ?>" target="_blank">
+									<img src="<?php echo $additional_featured_image['thumb'] ?>" alt="" class="w-100">
+								</a>
+							</div>
+						<?php endforeach; ?>
+						</div>
+					<?php
+					}
+					?>
+				</div>
+			</div>
+
+		<?php endif; ?>
 
 		<?php
-		wp_link_pages( array(
+		/*wp_link_pages( array(
 			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
 			'after'  => '</div>',
-		) );
+		) );*/
 		?>
 
 	</div><!-- .entry-content -->
