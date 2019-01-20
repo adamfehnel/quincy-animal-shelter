fff1f2652ed711a3e32121a0038bcc57408ddbb4
Adam Fehnel <link293@gmail.com>
2019-01-20 10:59:57 -0500

    I broke everything the last commit, this is better.
    Signed-off-by: Adam Fehnel <link293@gmail.com>

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -102,40 +102,40 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 				</div>
 
+			<?php endif; ?>
+
+		<?php else: ?>
+
+			<?php if ( !has_post_thumbnail() ) : ?>
+				<?php the_content(); ?>
 			<?php else: ?>
 
-				<?php if ( !has_post_thumbnail() ) : ?>
-					<?php the_content(); ?>
-				<?php else: ?>
-
-					<div class="row">
-						<div class="col-md-7">
-							<?php the_content(); ?>
-						</div>
-						<div class="col-md-5">
-							<?php echo get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'w-100' ) ); ?>
-
-							<?php 
-							if ( class_exists('Dynamic_Featured_Image') ) {
-								global $dynamic_featured_image;
-								$additional_featured_images = $dynamic_featured_image->get_featured_images( );
-								?>
-								<div class="row">
-								<?php foreach( $additional_featured_images as $additional_featured_image ): ?>
-									<div class="col-6 mt-4">
-										<a href="<?php echo $additional_featured_image['full'] ?>" target="_blank">
-											<img src="<?php echo $additional_featured_image['thumb'] ?>" alt="" class="w-100">
-										</a>
-									</div>
-								<?php endforeach; ?>
-								</div>
-							<?php
-							}
-							?>
-						</div>
+				<div class="row">
+					<div class="col-md-7">
+						<?php the_content(); ?>
 					</div>
+					<div class="col-md-5">
+						<?php echo get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'w-100' ) ); ?>
 
-				<?php endif; ?>
+						<?php 
+						if ( class_exists('Dynamic_Featured_Image') ) {
+							global $dynamic_featured_image;
+							$additional_featured_images = $dynamic_featured_image->get_featured_images( );
+							?>
+							<div class="row">
+							<?php foreach( $additional_featured_images as $additional_featured_image ): ?>
+								<div class="col-6 mt-4">
+									<a href="<?php echo $additional_featured_image['full'] ?>" target="_blank">
+										<img src="<?php echo $additional_featured_image['thumb'] ?>" alt="" class="w-100">
+									</a>
+								</div>
+							<?php endforeach; ?>
+							</div>
+						<?php
+						}
+						?>
+					</div>
+				</div>
 
 			<?php endif; ?>
 
